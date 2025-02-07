dc91ae667e09a75f831bd2868a078bacb188f7c5
Segward <gustask@stud.ntnu.no>
2025-02-07 13:16:24 +0100

    Added dependencies and plugins for the maven project

--- src/main/java/edu/ntnu/idat2003/Main.java
+++ src/main/java/edu/ntnu/idat2003/Main.java
@@ -1,7 +1,24 @@
 package edu.ntnu.idat2003;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello, JavaFX!");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 300, 200);
+
+        primaryStage.setTitle("Simple JavaFX Window");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
     }
 }
