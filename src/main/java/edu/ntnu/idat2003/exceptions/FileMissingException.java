6926b383734f349690cbfe21d79ddd32704f3c86
Trym Pedersen <trymolaf@gmail.com>
2025-03-26 15:01:44 +0100

    changed e to exception

--- src/main/java/edu/ntnu/idat2003/exceptions/FileMissingException.java
+++ src/main/java/edu/ntnu/idat2003/exceptions/FileMissingException.java
@@ -1,8 +1,8 @@
 package edu.ntnu.idat2003.exceptions;
 
 public class FileMissingException extends Exception{
-    public FileMissingException(String e) {
-        super(e);
+    public FileMissingException(String exception) {
+        super(exception);
     }
     
 }
