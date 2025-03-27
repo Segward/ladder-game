70418ccebbb8eb8549b33f1be32ef3658f3a4a43
Trym Pedersen <trymolaf@gmail.com>
2025-03-27 14:17:07 +0100

    Added java docs to dice class

--- src/main/java/edu/ntnu/idat2003/models/Dice.java
+++ src/main/java/edu/ntnu/idat2003/models/Dice.java
@@ -3,10 +3,24 @@ package edu.ntnu.idat2003.models;
 public class Dice {
   private int die;
 
+  /**
+   *  Constructor for dice class,
+   *  uses given int to set times a dice will be rolled.
+   * 
+   *  @param die int representing amount of throws
+   */
   public Dice(int die) {
     this.die = die;
   }
 
+  /**
+   *  Method for rolling a dice object,
+   *  constructs a new dice with 6 sides
+   *  and throws the amount of times die is defined.
+   *  For each throw the side value is added to a total.
+   * 
+   *  @return int representing total of all throws.
+   */
   public int roll() {
     int result = 0;
     Die die = new Die(6);
@@ -16,6 +30,11 @@ public class Dice {
     return result;
   }
 
+  /**
+   *  Getter for dice throw amount.
+   * 
+   *  @return int representing amount of throws
+   */
   public int getDie() {
     return die;
   }
