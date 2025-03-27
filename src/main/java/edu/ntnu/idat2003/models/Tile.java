fe1dd495edf0add021992556a855353722965039
Trym Pedersen <trymolaf@gmail.com>
2025-03-27 15:33:39 +0100

    Added java cod comments top tile class

--- src/main/java/edu/ntnu/idat2003/models/Tile.java
+++ src/main/java/edu/ntnu/idat2003/models/Tile.java
@@ -6,28 +6,64 @@ public class Tile {
   private TileAction action;
   private int position;
 
+  /**
+   *  Constructor for tile class,
+   *  defines the tiles position on the bord
+   *  and action based on parameters.
+   * 
+   *  @param position int representing tile position
+   *  @param action tileaction object
+   */
   public Tile(int position, TileAction action) {
     this.action = action;
     this.position = position;
   }
 
+  /**
+   *  Constructor for tile class,
+   *  only defines tile position on bord
+   *  based on parameter.
+   * 
+   *  @param position int representing tile position 
+   */
   public Tile(int position) {
     this.action = null;
     this.position = position;
   }
 
+  /**
+   *  Getter for next tiles position.
+   * 
+   *  @return int representing next tiles position
+   */
   public int getNextTile() {
     return position + 1;
   }
 
+  /**
+   *  Cheacks if tile object has a tile action.
+   * 
+   *  @return true if tileAction is not null, false if not
+   */
   public boolean hasAction() {
     return action != null;
   }
 
+  /**
+   *  Getter for tile action,
+   *  action that effects player on tile.
+   * 
+   *  @return tile action object
+   */
   public TileAction getAction() {
     return action;
   }
 
+  /**
+   *  Getter for tile position on bord.
+   * 
+   *  @return int representing tile position
+   */
   public int getPosition() {
     return position;
   }
