0b7230ca2b4bface0355e889cc59cdc1766b7117
Segward <gustask@stud.ntnu.no>
2025-03-27 12:39:40 +0100

    made some methods for player, board and figure selection

---
+++ src/main/java/edu/ntnu/idat2003/services/LadderGameService.java
@@ -0,0 +1,28 @@
+package edu.ntnu.idat2003.services;
+
+import javafx.scene.layout.FlowPane;
+import edu.ntnu.idat2003.models.Board;
+import edu.ntnu.idat2003.models.Figure;
+import edu.ntnu.idat2003.models.Player;
+import java.util.HashSet;
+
+public class LadderGameService {
+  private final StateService stateService = new StateService();
+  private final GsonService gsonService = new GsonService();
+  private final SelectionService selectionService = new SelectionService();
+
+  public void getPlayers(FlowPane flowPane) {
+    HashSet<Player> players = gsonService.getPlayers();
+    selectionService.createPlayerSelection(flowPane, players);
+  }
+
+  public void getBoards(FlowPane flowPane) {
+    HashSet<Board> boards = gsonService.getBoards();
+    selectionService.createBoardSelection(flowPane, boards);
+  }
+
+  public void getFigures(FlowPane flowPane) {
+    HashSet<Figure> figures = gsonService.getFigures();
+    selectionService.createFigureSelection(flowPane, figures);
+  }
+}
---
+++ src/main/java/edu/ntnu/idat2003/services/SelectionService.java
@@ -0,0 +1,22 @@
+package edu.ntnu.idat2003.services;
+
+import edu.ntnu.idat2003.models.Player;
+import edu.ntnu.idat2003.models.Board;
+import edu.ntnu.idat2003.models.Figure;
+import javafx.scene.layout.FlowPane;
+import java.util.HashSet;
+
+public class SelectionService {
+  public void creaetPlayerSelection(FlowPane flowPane, HashSet<Player> players) {
+  
+  }
+
+  public void createBoardSelection(FlowPane flowPane, HashSet<Board> boards) {
+  
+  }
+
+  public void createFigureSelection(FlowPane flowPane, HashSet<Figure> figures) {
+  
+  }
+}
+
