85749613c2cc15a4245d9a1a60bd66a74c39af6e
Segward <gustask@stud.ntnu.no>
2025-04-02 12:44:23 +0200

    Redirect from board selection to ladder game

--- src/main/java/edu/ntnu/idat2003/components/LadderGame.java
+++ src/main/java/edu/ntnu/idat2003/components/LadderGame.java
@@ -19,20 +19,14 @@ import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
 public class LadderGame {
-  // private static FlowPane sideMenu;
-
   /**
-   * Initializes game panes and children.
+   * Initializes game panes and board.
    *
-   * @param root Main oane
+   * @param root Main pane
    */
   public static void init(Pane root) {
-    // Clear the stack pane
     root.getChildren().clear();
 
-    // Create panes for laddergame
-
-    // Main pain where bord, player game menu assets are displayed
     FlowPane menuPane = new FlowPane();
     menuPane.prefWidthProperty().bind(root.widthProperty());
     menuPane.prefHeightProperty().bind(root.heightProperty());
@@ -41,152 +35,9 @@ public class LadderGame {
     menuPane.setOrientation(Orientation.VERTICAL);
     root.getChildren().add(menuPane);
 
-    // Pick the players
-    pickPlayers(root, menuPane);
-  }
-
-  /**
-   * Pick player or bord menu.
-   *
-   * @param root Main pain
-   * @param menuPane flowpane box
-   */
-  public static void pickPlayers(Pane root, FlowPane menuPane) {
-    // Clear the flow pane
-    menuPane.getChildren().clear();
-
-    // Create the content
-    Button addPlayerButton = new Button("Add player");
-    menuPane.getChildren().add(addPlayerButton);
-
-    Button pickBoardButton = new Button("Pick board");
-    menuPane.getChildren().add(pickBoardButton);
-
-    // Load the players
-    String filePath = "src/main/resources/players.json";
-    HashSet<Player> players = DataStorage.getPlayers(filePath);
-
-    for (Player player : players) {
-      FlowPane playerFlowPane = new FlowPane();
-      menuPane.getChildren().add(playerFlowPane);
-      playerFlowPane.setOrientation(Orientation.HORIZONTAL);
-
-      Text text = new Text(player.getName());
-      playerFlowPane.getChildren().add(text);
-
-      Button deletePlayerButton = new Button("Delete player");
-      playerFlowPane.getChildren().add(deletePlayerButton);
-
-      // Event handler
-      deletePlayerButton.setOnAction(
-          e -> {
-            deletePlayer(player);
-            pickPlayers(root, menuPane);
-          });
-    }
-
-    // Event handler
-    addPlayerButton.setOnAction(e -> addPlayer(root, menuPane));
-    pickBoardButton.setOnAction(e -> pickBoard(root, menuPane));
-  }
-
-  /**
-   * Add player menu.
-   *
-   * @param root Main pane
-   * @param menuPane Flowpane box
-   */
-  public static void addPlayer(Pane root, FlowPane menuPane) {
-    // Clear the flow pane
-    menuPane.getChildren().clear();
-
-    // Create the content
-    TextField textField = new TextField();
-    menuPane.getChildren().add(textField);
-    textField.setPromptText("Enter player name");
-
-    Button pickFigureButton = new Button("Pick figure");
-    menuPane.getChildren().add(pickFigureButton);
-    pickFigureButton.setOnAction(e -> pickFigure(root, menuPane, textField.getText()));
-  }
-
-  private static void deletePlayer(Player player) {
-    // Delete the player
-    String filePath = "src/main/resources/players.json";
-    DataStorage.deletePlayer(player, filePath);
-  }
-
-  public static void pickFigure(Pane root, FlowPane menuPane, String playerName) {
-    // Clear the flow pane
-    menuPane.getChildren().clear();
-
-    // Create the content
-    Button addPlayerButton = new Button("Finish adding player");
-    menuPane.getChildren().add(addPlayerButton);
-
-    // Event handler
-    addPlayerButton.setOnAction(
-        e -> {
-          savePlayer(playerName, "Figure");
-          pickPlayers(root, menuPane);
-        });
-  }
-
-  private static void savePlayer(String playerName, String figure) {
-    // Save the player
-    String filePath = "src/main/resources/players.json";
-    Figure figureObject = new Figure(figure);
-    Player player = new Player(playerName, figureObject);
-    DataStorage.savePlayer(player, filePath);
-  }
-
-  /**
-   * Menu for picking bord.
-   *
-   * @param root main pane
-   * @param menuPane main menu pane
-   */
-  public static void pickBoard(Pane root, FlowPane menuPane) {
-    // Clear the flow pane
-    menuPane.getChildren().clear();
-
-    // Create the content
-    Button playGameButton = new Button("Play game");
-    menuPane.getChildren().add(playGameButton);
-
-    // Load the boards
-
-    // Save crappy board to test datastorage
-    Board board = new Board("Mlem");
-    for (int i = 0; i < 90; i++) {
-      Tile tile = new Tile(i);
-      board.addTile(i, tile);
-    }
-
-    String filePath = "src/main/resources/boards.json";
-    DataStorage.saveBoard(board, filePath);
-
-    // Event handler
-    playGameButton.setOnAction(e -> playGame(root, menuPane));
-  }
-
-  /**
-   * Initializes game panes and children.
-   *
-   * @param root main pane
-   * @param menuPane main menu pane
-   */
-  public static void playGame(Pane root, FlowPane menuPane) {
-    // Clear the flow pane
-    menuPane.getChildren().clear();
-
-    // Create the content
     HBox hBox = new HBox();
     root.getChildren().add(hBox);
 
-    // Create the sections
-
-    // Main panal where bord game is displayed
     FlowPane boardPane = new FlowPane();
     hBox.getChildren().add(boardPane);
     boardPane.setStyle("-fx-background-color: WHITE;");
@@ -195,7 +46,6 @@ public class LadderGame {
     boardPane.setOrientation(Orientation.VERTICAL);
     boardPane.setAlignment(Pos.CENTER);
 
-    // Side panal/menu
     FlowPane playerPane = new FlowPane();
     hBox.getChildren().add(playerPane);
     playerPane.setStyle("-fx-background-color: beige;");
@@ -203,7 +53,6 @@ public class LadderGame {
     playerPane.prefHeightProperty().bind(root.heightProperty());
     playerPane.setOrientation(Orientation.VERTICAL);
 
-    // Trym Experiment grid
     GridPane bord = new GridPane();
     bord.setPadding(new Insets(5, 5, 5, 5));
     bord.setVgap(5);
@@ -237,18 +86,7 @@ public class LadderGame {
     Button rollDiceButton = new Button("Roll Dice");
     playerPane.getChildren().add(rollDiceButton);
 
-    // Event handler
     rollDiceButton.setOnAction(e -> LadderGameEngine.rollDice(text));
-    endGameButton.setOnAction(e -> endGame(root));
-  }
-
-  /**
-   * Takes you to start menu of application.
-   *
-   * @param root main pain
-   */
-  private static void endGame(Pane root) {
-    // End the game
-    MainFrame.init(root);
+    endGameButton.setOnAction(e -> MainFrame.init(root));
   }
 }
--- src/main/java/edu/ntnu/idat2003/views/UserInterface.java
+++ src/main/java/edu/ntnu/idat2003/views/UserInterface.java
@@ -16,44 +16,19 @@ public class UserInterface {
   }
 
   public void init() {
-    // Set the root Pane as the scene of the primaryStage
     primaryStage.setTitle("Ladder Game");
-    primaryStage.setMinWidth(400);
-    primaryStage.setMinHeight(400);
+    primaryStage.setMinWidth(1000);
+    primaryStage.setMinHeight(600);
 
-    // Bind the root Pane to the primaryStage's size
     root.prefWidthProperty().bind(primaryStage.widthProperty());
     root.prefHeightProperty().bind(primaryStage.heightProperty());
 
-    // Add the Stylesheet to the root Pane
     root.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
     root.setId("root");
   }
 
   public void start() {
-    /*
-    //Trym Experiment grid
-    GridPane bord = new GridPane();
-    bord.setMinSize(500, 500);
-    bord.setPadding(new Insets(10, 10, 10, 10));
-    bord.setVgap(10);
-    bord.setHgap(10);
-    bord.setAlignment(Pos.CENTER);
-
-    for(int i = 0;i<10;i++) {
-        for(int j = 0; j<10;j++) {
-            Rectangle box = new Rectangle();
-            box.setWidth(50);
-            box.setHeight(50);
-            box.setFill(Color.LIGHTGREEN);
-            bord.add(box, i,j);
-        }
-    }
-     */
-    // Initialize the MainFrame
     MainFrame.init(root);
-
-    // Scene scene = new Scene(bord, 1000, 800);
     primaryStage.setScene(new Scene(root));
     primaryStage.show();
   }
