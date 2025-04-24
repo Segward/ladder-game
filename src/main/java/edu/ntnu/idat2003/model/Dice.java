85a8232f70556e85c07472a26c5d3d20d96e871c
Trym Pedersen <trymolaf@gmail.com>
2025-04-24 13:20:36 +0200

    Added detailed javaDcos to the Dice Class

--- src/main/java/edu/ntnu/idat2003/model/Dice.java
+++ src/main/java/edu/ntnu/idat2003/model/Dice.java
@@ -3,10 +3,23 @@ package edu.ntnu.idat2003.model;
 public class Dice {
   private int die;
 
+  /**
+   *  Constructor for the dice class.
+   *  Uses the int parameter to set the dice amount.
+   * 
+   *  @param die int representing the dice amount.
+   */
   public Dice(int die) {
     this.die = die;
   }
 
+  /**
+   *  Method for rolling the dice.
+   *  Creates a new die object with 6 sides and
+   *  rolls the die corresponding to the die variable amount.
+   * 
+   *  @return result of all rolled dice
+   */
   public int roll() {
     int result = 0;
     Die die = new Die(6);
@@ -16,6 +29,11 @@ public class Dice {
     return result;
   }
 
+  /**
+   *  Getter for dice amount.
+   *  
+   * @return int representing the dice amount
+   */
   public int getDie() {
     return die;
   }
