b0d6d859a49754855a511abe8983b2d061204b3e
Trym Pedersen <trymolaf@gmail.com>
2025-05-19 13:15:39 +0200

    Added javadoc to the Figure class

--- src/main/java/edu/ntnu/idat2003/model/player/Figure.java
+++ src/main/java/edu/ntnu/idat2003/model/player/Figure.java
@@ -1,17 +1,43 @@
 package edu.ntnu.idat2003.model.player;
 
+/**
+ *  Class representing player figure.
+ *  Consists of methods that defines this object.
+ */
 public class Figure {
 
   private String name;
 
+  /**
+   *  Constructor for the Figure class.
+   *  Takes an String parameter and sets
+   *  that as the Figures name.
+   * 
+   *  @param name String representing figure name
+   */
   public Figure(String name) {
     this.name = name;
   }
 
+  /**
+   *  Getter for the Figure name.
+   *  Retreves the name String variable value.
+   * 
+   * @return String representing figure name
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   *  Method for checking if an object is equal to
+   *  this spesific Figure object. Checks if the 
+   *  parameter object is the same as this object.
+   *  Checks both for object value and if string name
+   *  value is equal.
+   * 
+   *  @return true if objects are the same, false if not
+   */
   @Override
   public boolean equals(Object obj) {
     if (obj == this) {
@@ -31,6 +57,13 @@ public class Figure {
     return false;
   }
 
+  /**
+   *  Method for reducing collision with similar names.
+   *  Uses a hash Code method on the String name value to 
+   *  generate a hash code of the name, then multiplies that
+   *  value wiht 31 to reduce the likelihood of collision.
+   * 
+   */
   @Override
   public int hashCode() {
     int result = 31 * name.hashCode();
