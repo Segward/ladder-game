9375e160b8db39cc1942ebf9e89cf2fcbf11d1a1
Trym Pedersen <trymolaf@gmail.com>
2025-05-19 12:42:22 +0200

    Added javadoc to the dice class

--- src/main/java/edu/ntnu/idat2003/model/dice/Dice.java
+++ src/main/java/edu/ntnu/idat2003/model/dice/Dice.java
@@ -1,12 +1,32 @@
 package edu.ntnu.idat2003.model.dice;
 
+/**
+ *  Class representing the amount of dice to be rolled.
+ *  Includes a method for rolling the dice.
+ */
 public class Dice {
   private int die;
 
+  /**
+   *  Constructor for the dice class.
+   *  Takes an int parameter and sets 
+   *  that as the number of dice to be rolled.
+   * 
+   * @param die int representing dice amount
+   */
   public Dice(int die) {
     this.die = die;
   }
 
+  /**
+   *  Method for rolling all of the dice.
+   *  Creates a new die object and result int.
+   *  Utilizes a for loop that loops equal to this
+   *  dice objects int die value. Adds each consecutive
+   *  int result to the new result int. 
+   * 
+   *  @return int representing the dice side amount.
+   */
   public int roll() {
     int result = 0;
     Die die = new Die(6);
@@ -16,6 +36,13 @@ public class Dice {
     return result;
   }
 
+  /**
+   *  Getter for the die amount.
+   *  Retreves the an int representing the
+   *  amount of dice to be rolled.
+   * 
+   *  @return int representing dice amount
+   */
   public int getDie() {
     return die;
   }
