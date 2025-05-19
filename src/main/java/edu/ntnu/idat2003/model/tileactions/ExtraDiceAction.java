4c9ffafea6063f63013a7d0602e55b58b8dd39ff
Trym Pedersen <trymolaf@gmail.com>
2025-05-19 13:50:30 +0200

    Added javadoc to the ExtraDiceAction class

--- src/main/java/edu/ntnu/idat2003/model/tileactions/ExtraDiceAction.java
+++ src/main/java/edu/ntnu/idat2003/model/tileactions/ExtraDiceAction.java
@@ -3,12 +3,31 @@ package edu.ntnu.idat2003.model.tileactions;
 import edu.ntnu.idat2003.model.Vector2;
 import edu.ntnu.idat2003.model.player.Player;
 
+/**
+ *  Class representing the extra dice action.
+ *  Incluedes method for defining if action is active.
+ * 
+ */
 public class ExtraDiceAction extends TileAction {
 
+  /**
+   *  Constuctor for ExtraDiceAction inherited from TileAction.
+   *  Takes a Vactor2 object and sets that as the tileAction position. 
+   * 
+   *  @param start Vector2 representing tileAction position
+   */
   public ExtraDiceAction(Vector2 start) {
     super(start);
   }
 
+  /**
+   *  Method for setting player extraDice to true.
+   *  Utilizes the setter method in the player class on the
+   *  player object parameter to set the players extra 
+   *  die value to true.
+   * 
+   *  @param player player object to recive an extra die.
+   */
   @Override
   public void execute(Player player) {
     player.setExtraDice(true);
