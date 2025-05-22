c02272e5ed147285f5a955bcf2ac849d53689dc0
Trym Pedersen <trymolaf@gmail.com>
2025-05-22 17:14:26 +0200

    Added javadoc to FigureWriter class

--- src/main/java/edu/ntnu/idat2003/io/FigureWriter.java
+++ src/main/java/edu/ntnu/idat2003/io/FigureWriter.java
@@ -6,10 +6,23 @@ import edu.ntnu.idat2003.exception.DataWriteException;
 import edu.ntnu.idat2003.model.Figure;
 import edu.ntnu.idat2003.util.CsvUtil;
 
+/**
+ *  Class for Writting Figure csv files.
+ *  Inclueds method for writting Figure objects to csv.
+ */
 public class FigureWriter {
 
     private static final String path = "data/figure.csv";
 
+    /**
+     *  Method for saving figure objects to spesific csv file.
+     *  Takes HasSet<Figure> as parameter.
+     *  Using a for loop, saves all figure data as StringBuilder.
+     *  Then utilizing different string methods fomats string to csv format,
+     *  before utilising CsvUtil method to write data to spesific csv file.
+     * 
+     *  @param figures
+     */
     public static void saveFigures(HashSet<Figure> figures) {
     StringBuilder data = new StringBuilder();
     for (Figure figure : figures) {
