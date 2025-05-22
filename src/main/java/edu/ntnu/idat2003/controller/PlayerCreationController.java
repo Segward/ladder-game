6aac0b009bf5c561bb5dd685bad536602d859abf
Trym Pedersen <trymolaf@gmail.com>
2025-05-22 14:36:56 +0200

    Added javadoc to playerCreationController class

--- src/main/java/edu/ntnu/idat2003/controller/PlayerCreationController.java
+++ src/main/java/edu/ntnu/idat2003/controller/PlayerCreationController.java
@@ -17,28 +17,60 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
+/**
+ *  Class representing the playerCreation screen.
+ *  Incueds medthod for interaction with visual logic.
+ */
 public class PlayerCreationController {
 
   private final BorderPane root;
   private final HBox hBox;
   private final TextField playerName;
 
+  /**
+   *  Constructor for the PlayerCreationController.
+   *  Takes an BorderPane, HBox and TextFieald as Parameters.
+   * 
+   *  @param root BorderPane representing Main canvas
+   *  @param hBox HBox representing the player creator screen
+   *  @param playerName TextField where user creates player name
+   */
   public PlayerCreationController(BorderPane root, HBox hBox, TextField playerName) {
     this.root = root;
     this.hBox = hBox;
     this.playerName = playerName;
   }
 
+  /**
+   *  Initilases screen visauls and deffines return button functionality.
+   *  Takes Button as parameter, and deffinese onReturn() method as functionality,
+   *  before displaying available figures.
+   * 
+   *  @param returnButton Button to return to config screen
+   */
   public void init(Button returnButton) {
     returnButton.setOnAction(e -> onReturn());
     updateFigures();
   }
 
+  /**
+   *  Method for returning user to config screen.
+   *  Creates new Configuration object and initilases objects init() method.
+   */
   public void onReturn() {
     Configuration configuration = new Configuration(root);
     configuration.init();
   }
 
+   /**
+   *  Creates a StackPane for a singel figure display.
+   *  Creates a stackpane that holds a VBox that
+   *  holdes the Text player name and a button for selection,
+   *  and a image of the figure.
+   * 
+   *  @param figure figure object to be displayed
+   *  @return StackPane that inclued fiure data
+   */
   private StackPane createFigurePane(Figure figure) {
     StackPane figurePane = new StackPane();
     figurePane.setAlignment(Pos.CENTER);
@@ -66,6 +98,11 @@ public class PlayerCreationController {
     return figurePane;
   }
 
+    /**
+   *  Method for updating displayed figures.
+   *  Utilazes a for loop to go through each remaining figure
+   *  and creates a display pane for them. 
+   */
   private void updateFigures() {
     hBox.getChildren().clear();
     HashSet<Figure> figureSet = FigureReader.getAvailableFigures();
@@ -78,6 +115,16 @@ public class PlayerCreationController {
     }
   }
 
+  /**
+   *  Method for adding player to game.
+   *  Takes Figure as a parameter.
+   *  Checks if TextField is empty, if it is informes the player that is can not.
+   *  If not creates a new player with a vector2 object as position,
+   *  then adds new player to save player file.
+   *  Then creates new Configuration object and initilases objects init() method.
+   * 
+   *  @param figure
+   */
   public void addPlayer(Figure figure) {
     String name = playerName.getText();
     if (name.isEmpty()) {
