5fa9e0fac9e2c7a1c46f8dc29b9f845f44499934
Trym Pedersen <trymolaf@gmail.com>
2025-05-22 16:53:33 +0200

    Added javadoc to FigureFactory class

--- src/main/java/edu/ntnu/idat2003/io/FigureFactory.java
+++ src/main/java/edu/ntnu/idat2003/io/FigureFactory.java
@@ -3,7 +3,15 @@ package edu.ntnu.idat2003.io;
 import edu.ntnu.idat2003.model.Figure;
 import java.util.HashSet;
 
+/**
+ *  Class representing a factory where Figure objects are made.
+ */
 public class FigureFactory {
+  /**
+   *  Method that creates a set of Figures.
+   *  Hard codes 5 unique Figure objects and adds them to an HasSet,
+   *  before utilizing a FigureWritter method to save them as csv.
+   */
   public static void makeFigures() {
     HashSet<Figure> figures = new HashSet<>();
     figures.add(new Figure("Knight", "/figure/knight.png"));
